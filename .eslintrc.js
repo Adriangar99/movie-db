4f0bbdef47262eaada05eaef99fa56ae986cec1d
Adrián García <adriangargar99@gmail.com>
2022-11-04 19:55:47 +0000

    build: update dependencies and add storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-	},
-	extends: ['next', 'next/core-web-vitals', 'prettier'],
-	plugins: ['testing-library', 'jest-dom', 'unused-imports'],
-}
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: ['next', 'next/core-web-vitals', 'prettier', 'plugin:storybook/recommended'],
+  plugins: ['testing-library', 'jest-dom', 'unused-imports']
+};
